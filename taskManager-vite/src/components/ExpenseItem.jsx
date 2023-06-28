6cf92ff240ab908b0e267c10799843d52b8e2552
Enrique Lovera <kikelovera7@gmail.com>
2023-06-28 20:48:10 +0100

    adding card component and updating expense item

--- taskManager-vite/src/components/ExpenseItem.jsx
+++ taskManager-vite/src/components/ExpenseItem.jsx
@@ -1,15 +1,16 @@
 import './ExpenseItem.css';
+import Card from './Card';
 import ExpenseDateItem from './ExpenseDateItem';
 
 export default function ExpenseItem(props){
-    const {value} = props;
+    
     return (
-        <div className='expense-item'>
-            <ExpenseDateItem date={value.date}/>
+        <Card className='expense-item'>
+            <ExpenseDateItem date={props.value.date}/>
             <div className='expense-item__description'>
                 <h2>{props.value.title}</h2>
                 <div className='expense-item__price'>{props.value.amount}</div>
             </div>
-        </div>
+        </Card>
     )
 }
