354da7a2ca4da2ddd2e0afcb8a2bb732d883174f
Enrique Lovera <enrique.lovera@globant.com>
2023-06-28 13:43:02 -0300

    adding an arrays of task for testing purposes, and displaying the expense Component

--- taskManager-vite/src/App.jsx
+++ taskManager-vite/src/App.jsx
@@ -1,10 +1,22 @@
-import { useState } from 'react'
 import './App.css'
+import ExpenseItem from './components/ExpenseItem'
 
 function App() {
+  const expenses = [
+    {title:'car insurance', date:new Date(), amount:1234.5},
+    {title:'supermarket ', date:new Date(), amount:445.5},
+    {title:'baby school', date:new Date(), amount:33331.5},
+    {title:'university loan', date:new Date(), amount:7866.5}
+
+  ]
   return (
     <>
       <div>HOLA MUNDOOOO DESDE REACT!!!</div>
+      {
+      expenses.map((x,i) => {
+        return <ExpenseItem key={i} value={x}></ExpenseItem>
+      })
+      }
     </>
   )
 }
